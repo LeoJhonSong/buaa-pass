ae58e0817485a71394a11668adbc1ed0aebf6187
LeoJhon.Song <leojhon.song@outlook.com>
2022-09-03 12:49:20 +0800

    re-implement by pure html and js

---
+++ PassCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+
+</script>
+
+<template>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dateTime: "",
+      name: name,
+    };
+  },
+  methods: {
+    dateTimeTicker: function () {
+      var self = this;
+      // var avatarUrl = new URL("/imgs/avatar/" + avatar + ".jpg", import.meta.url).href;
+      // if (!this.isHome) {
+      //   document.getElementById("avatar").src = avatarUrl;
+      // }
+    },
+  },
+  mounted() {
+    this.dateTimeTicker();
+  },
+};
+</script>
+
+<style>
+</style>
